8dd0c0a53d5d9861d9fa4e15040b8f4fbb6a0a0f
Sonhaji <ihazsonhaji95@gmail.com>
2021-07-10 16:43:31 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,14 +2,21 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+//songs
 const songs = require('./api/songs');
 const SongsValidator = require('./validator/songs');
 const SongsService = require('./services/postgres/SongsService');
 const ClientError = require('./exceptions/ClientError');
 
+//users
+const user = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+const users = require('./api/users');
 
 const init = async () => {
     const songsService = new SongsService();
+    const usersService = new UsersService();
     
     const server = Hapi.server({
         port: process.env.PORT,
@@ -20,8 +27,8 @@ const init = async () => {
             },
         },
     });
-    /* membuat extentions function untuk life cyle onPreResponse, dimana ia akan mengintervensi response
-    sebelum dikirimkan ke client. Bisa menetapkan error handling bila response tersebut merupakan Client error */
+    
+    /* membuat extentions function untuk life cyle onPreResponse */
     server.ext('onPreResponse', (request, h) => {
         //mendapatkan konteks response dari request
         const { response } = request;
@@ -39,16 +46,23 @@ const init = async () => {
         /*jika bukan ClientError, lanjutkan dgn response sebelumnya (tanpa terintervensi)*/
         return response.continue || response;
     });
-    /* Dgn begitu, di handler , bisa fokus terhadap logika dalam menangani request
-    tanpa adanya error handling */
 
-    await server.register({
-        plugin: songs,
-        options: {
+    await server.register([
+        {
+            plugin: songs,
+            options: {
             service: songsService,
             validator: SongsValidator,
+            }
         },
-    });
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            }
+        },
+    ]);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
